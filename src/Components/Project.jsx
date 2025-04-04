768aa0d21240371696d524366ff9db97ee259ceb
matinOlalek <matinOlalek@gmail.com>
2025-04-04 22:45:38 +0100

    update

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -9,7 +9,7 @@ const projectsData = [
     id: "1",
     title: "WanderLust",
     description: "A travel agency web app helping users book trips, explore destinations, and manage travel plans.",
-    technologies: ["HTML/CSS/Javascript", "Bootstrap"],
+    technologies: ["Html/Css/Javascript", "Bootstrap"],
     liveUrl: "https://thewanderlusttravels.com/",
     githubUrl: "https://github.com/example/taskmanager"
   },
